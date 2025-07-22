a1d8b75cb21665046021845a84ad52ee9e91398c
Emo-77 <emirip11@gmail.com>
2025-07-22 10:56:21 +0300

    Lumina Başlangıç

--- fake_insta/src/App.js
+++ fake_insta/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Header from './Header';
+
+// const URL = "https://jsonplaceholder.typicode.com"
+// const UserId = 2
 
 function App() {
 
+  // const getAllUsers = async ()=>{
+  //   const response = await axios.get(`${URL}/posts`)
+  //   console.log(response.data)
+  // }
+
+  // const getPostByUserId = async (userId) => {
+  //   const response = await axios.get(`${URL}/posts?userId=${UserId}`)
+  //   console.log(response.data)
+  // }
+
+  // useEffect(()=>{
+  //   getAllUsers();
+  //   getPostByUserId(UserId)
+  // },[])
+
   return (
-    <></>
+    <>
+      <Header/>
+
+
+    </>
+
   );
 }
 
---
+++ fake_insta/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <>
+
+            <div className="Header"><div className="HeaderImage"></div></div>
+
+        </>
+    );
+}
+
+export default Header;
